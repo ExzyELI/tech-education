4c7e61260a8cfc2c7004d0b8cf1057a7d8a1154a
usmansaiyed02 <saiyedusman@gmail.com>
2024-02-01 01:53:00 -0500

    bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <main>
     <div className="flex flex-col min-h-screen bg-white">
       <header className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
         <div className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-[#afce8b] flex justify-between items-center px-4 py-5"> 
@@ -15,7 +16,7 @@ export default function Home() {
         </div>
       </header>
       <section className="absolute top-20 w-full text-center ">
-        <h1 className="text-orange-500 text-5xl font-bold py-1 ">Welcome to Tech Education!</h1>
+        <h1 className="text-orange-500 text-5xl font-bold py-1 tracking-tight md:text-5xl lg:mb-16 lg:ml-[80px] lg:text-5xl ">Welcome to Tech Education!</h1>
         <h2 className="text-black text-5xl font-bold">...</h2>
         </section>
         <div className="cursor-pointer flex justify-center mt-[220px] ">
@@ -37,7 +38,8 @@ export default function Home() {
           </span>
         </div>
       </footer>
-    </div>
+      </div>
+      </main>
   );
 }
 
