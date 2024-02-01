c0624a3986ae0a8263cd3b268653d4d1c234260a
Andre-N <andrehadnan@yahoo.com>
2024-02-01 03:47:21 -0500

    Added temp activities and grades added color styling ActivityScroll.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import "./ActivityScroll.css"
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
@@ -16,14 +16,52 @@ export default function Home() {
 
 
       <section className="container mx-auto py-20">
-        <h1 className="text-orange-500 text-7xl font-bold py-1">Activity Grades</h1>
-        <h2 className="text-black opacity-20 text-5xl font-bold">Coming soon</h2>
-        </section>
+        <h1 className="text-orange-500 text-7xl font-bold py-1">Grades</h1>
+        <h2 className="text-black opacity-20 text-5xl font-bold">Student name</h2>
+        <div className="flex justify-between mt-10">
+          <div className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+            <h3 className="text-xl font-semibold mb-3">Activities</h3>
 
-    
+            <ul>
+              {/* Activity colors for odds and evens*/}
+              <div className="activity-box">this is a long name for an activiy for testing purposes</div>
+              <div className="activity-box">Activity 2</div>
+              <div className="activity-box">Activity 3</div>
+              <div className="activity-box">Activity 4</div>
+              <div className="activity-box">Activity 5</div>
+              <div className="activity-box">Activity 6</div>
+              <div className="activity-box">Activity 7</div>
+              <div className="activity-box">Activity 8</div>
+              <div className="activity-box">Activity 9</div>
+              <div className="activity-box">Activity 10</div>
+            </ul>
+          </div>
+          <div className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+            <h3 className="text-xl font-semibold mb-3">Grades</h3>
+            <ul>
+              {/* Wrap each grade in a div with a custom class */}
+              <div className="grade-box">5/10</div>
+              <div className="grade-box">10/10</div>
+              <div className="grade-box">5/10</div>
+              <div className="grade-box">10/10</div>
+              <div className="grade-box">5/10</div>
+              <div className="grade-box">10/10</div>
+              <div className="grade-box">5/10</div>
+              <div className="grade-box">10/10</div>
+              <div className="grade-box">5/10</div>
+              <div className="grade-box">10/10</div>
+              
+            </ul>
+          </div>
+        </div>
+      </section>
+
+
+
+
 
       <footer className="bg-gray-800 text-white py-8 mt-auto">
-      <div className="mx-auto flex w-full max-w-screen-xl items-center justify-center px-4 py-4">
+        <div className="mx-auto flex w-full max-w-screen-xl items-center justify-center px-4 py-4">
           <span className="text-sm sm:text-center ">
             © 2024 Tech Education™
           </span>
