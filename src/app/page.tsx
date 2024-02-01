c44adcf87a7e88ace045400c08e12fbbea9d65e7
usmansaiyed02 <saiyedusman@gmail.com>
2024-01-31 22:56:07 -0500

    header theme matched

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-      <header className="bg-white text-black py-3">
-        <div className="container mx-auto flex justify-between items-center">
+      <header className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+        <div className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-[#afce8b] flex justify-between items-center px-4 py-2">
           <h1 className="cursor-pointer text-xl font-bold hover:text-2xl transition-all duration-800 ">Tech Education</h1>
           <div className="flex text-lg">
             <label className="mr-5 cursor-pointer transition-all duration-500 transform hover:-translate-y-1 hover:bg-orange-500 hover:text-white hover:p-1 hover:rounded">Activities</label>
