596eae76775e989bd642bb5647ae7c407f689452
usmansaiyed02 <saiyedusman@gmail.com>
2024-02-01 13:33:22 -0500

    code organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,67 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-    <div className="flex flex-col min-h-screen bg-white">
+    <main className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+      <title>Tech Education</title>
+      {/*navbar begins */}
+    <nav className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-[#afce8b]">
       <header className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
-        <div className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-[#afce8b] flex justify-between items-center px-4 py-5"> 
-          <h1 className="cursor-pointer text-xl font-bold ">Tech Education</h1>
-          <nav className="text-2lg">
-            <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Activities</a>
-            <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Grade</a>
-            <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Reports</a>
-            <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Profile</a>
-          </nav>
+        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+          <span className="self-center whitespace-nowrap text-2xl font-semibold"> Tech Education
+            </span>
+            {/* tabs */}
+            <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+              <nav className="text-2lg">
+                <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Activities</a>
+                <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Grade</a>
+                <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Reports</a>
+                <a href="#" className="text-[#132241] cursor-pointer text-lg px-3 font-semibold hover:text-[#5c6ac4]">Profile</a>
+                </nav>
+            </div>
         </div>
       </header>
-      <section className="absolute top-20 w-full text-center ">
-        <h1 className="text-black text-5xl font-bold py-1 tracking-tight md:text-5xl lg:mb-16 lg:ml-[80px] lg:text-5xl ">Welcome to Tech Education!</h1>
-        <h2 className="text-black text-5xl font-bold">...</h2>
-        </section>
-        <div className="cursor-pointer flex justify-center mt-[220px] ">
-        <a href="#" className="w-1/4 h-[300px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform">
-          <h1 className="text-3xl text-center font-semibold ">Kindergarden</h1>
-        </a>
-        <a href="#" className="w-1/4 h-[300px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform">
-          <h1 className="text-3xl text-center font-semibold">1st Grade</h1>
-        </a>
-        <a href="#" className="w-1/4 h-[300px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform">
-          <h1 className="text-3xl text-center font-semibold">2nd Grade</h1>
-        </a>
-      </div>
+      </nav>
+      {/* navbar ends */}
 
-      <footer className="bg-[#ffe08d]">
-        <div className="mx-auto flex w-full max-w-screen-xl items-center justify-center px-4 py-4">
+      {/* banner begins */}
+      <section className="-mb-[105px] border-b bg-[#ffecde] lg:-mt-5">
+        <div className="px-6 py-12 text-center md:px-12 lg:my-12 lg:text-left">
+          <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+            <div className="grid items-center gap-12 lg:grid-cols-2">
+              <div className="mt-12 lg:mt-[105px]">
+                {/* title */}
+                <h1 className="text-6xl font-bold tracking-tight md:text-5xl lg:mb-16 lg:ml-[80px] lg:text-5xl">
+                  Welcome to Tech Education!
+                  </h1>
+                  </div>
+                  {/* title */}
+                  <div className="cursor-pointer flex justify-center items-center py-2 bg-white">
+                    <a href="#" className="w-1/4 h-[280px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform flex justify-center items-center">
+                      <h1 className="text-3xl text-center font-semibold tracking-tight">Kindergarten</h1>
+                      </a>
+                      <a href="#" className="w-1/4 h-[280px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform flex justify-center items-center">
+                        <h1 className="text-3xl text-center font-semibold tracking-tight">1st <br />Grade</h1>
+                        </a>
+                        <a href="#" className="w-1/4 h-[280px] bg-[#e1f3ff] rounded-md m-2 hover:scale-105 transition-transform flex justify-center items-center">
+                          <h1 className="text-3xl text-center font-semibold tracking-tight">2nd <br />Grade</h1>
+                          </a>
+                          </div>
+                          </div>
+                          </div>
+                           </div>
+                           </section>
+      {/* banner ends */}
+
+      {/* footer begins */}
+      <footer className="fixed bottom-0 left-0 right-0 bg-[#afce8b]">
+        <div className=" mt-[100px] mx-auto flex w-full max-w-screen-xl items-center justify-center px-4 py-4">
           <span className="text-sm sm:text-center ">
             © 2024 Tech Education™
           </span>
         </div>
       </footer>
-      </div>
-      </main>
+      {/* footer ends */}
+    </main>
   );
 }
 
