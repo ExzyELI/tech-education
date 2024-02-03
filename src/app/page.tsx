31a73dc2a18dd53cd96caabfe7acd2a4aacf0b8d
majedau <majedaullah@gmail.com>
2024-02-03 12:44:19 -0500

    Moved sign out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,21 @@
-'use client'
+"use client";
 import Image from "next/image";
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {auth} from '@/app/firebase/init_app'
-import {useRouter} from "next/navigation";
-import {signOut} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/init_app";
+import { useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
 
 export default function Home() {
   const [user] = useAuthState(auth);
-  const userSession = sessionStorage.getItem('user');
+  const userSession = sessionStorage.getItem("user");
 
   console.log(user);
-  const router = useRouter()
-  if (!user && !userSession){
-    router.push('/sign-in')
+  const router = useRouter();
+  if (!user && !userSession) {
+    router.push("/sign-in");
   }
 
   return (
-    
-    //Mock sign-out button
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => {
-        signOut(auth)
-        sessionStorage.removeItem('user')
-      }
-    }>
-        Log out
-      </button>
     <main className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
       <title>Tech Education</title>
       {/* navbar begins */}
@@ -48,9 +38,18 @@ export default function Home() {
             >
               Sign Up
             </button>
+
+            {/* Mock sign-out button */}
+            <button
+              onClick={() => {
+                signOut(auth);
+                sessionStorage.removeItem("user");
+              }}
+            >
+              Log out
+            </button>
           </div>
         </div>
-        
       </nav>
       {/* navbar ends */}
 
