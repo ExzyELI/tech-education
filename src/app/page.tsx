ef9182a5ae5b0e95c7a61ae06363802cb565f104
majedau <majedaullah@gmail.com>
2024-02-04 15:06:59 -0500

    Repositioned the image and changed the colors to be more cohesive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <main className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+    <main className="font-family: font-serif leading-normal tracking-normal text-[#132241]">
       <title>Tech Education</title>
       {/*navbar begins */}
       <nav className="sticky w-full border-b border-gray-200 bg-[#afce8b]">
-        <header className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
+        <header className="font-family: font-serif leading-normal tracking-normal">
           <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
             <span className="self-center whitespace-nowrap text-2xl font-semibold">
               {" "}
@@ -15,25 +15,25 @@ export default function Home() {
               <nav className="text-2lg">
                 <a
                   href="#"
-                  className="cursor-pointer px-3 text-lg font-semibold text-[#132241] hover:text-[#5c6ac4]"
+                  className="cursor-pointer px-3 text-lg font-semibold hover:text-[#ffe08d]"
                 >
                   Activities
                 </a>
                 <a
                   href="#"
-                  className="cursor-pointer px-3 text-lg font-semibold text-[#132241] hover:text-[#5c6ac4]"
+                  className="cursor-pointer px-3 text-lg font-semibold hover:text-[#ffe08d]"
                 >
                   Grade
                 </a>
                 <a
                   href="#"
-                  className="cursor-pointer px-3 text-lg font-semibold text-[#132241] hover:text-[#5c6ac4]"
+                  className="cursor-pointer px-3 text-lg font-semibold hover:text-[#ffe08d]"
                 >
                   Reports
                 </a>
                 <a
                   href="#"
-                  className="cursor-pointer px-3 text-lg font-semibold text-[#132241] hover:text-[#5c6ac4]"
+                  className="cursor-pointer px-3 text-lg font-semibold hover:text-[#ffe08d]"
                 >
                   Profile
                 </a>
@@ -48,14 +48,32 @@ export default function Home() {
       <section className="border-b bg-[#ffecde]">
         <div className="px-6 pb-10 text-center md:px-12 lg:text-left">
           <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-            <header className="gradient flex w-full items-center justify-center py-3 font-serif text-6xl leading-normal tracking-normal text-[#132241] ">
+            <header className="flex w-full items-center justify-center py-3 font-serif text-5xl leading-normal tracking-normal">
               My Classroom
             </header>
 
+            <div className="col-span-1 row-span-1 mb-10">
+              <div className="flex items-center justify-center object-center">
+                <img
+                  className="h-[250px]"
+                  src="https://i.imgur.com/1EDuUBO.png"
+                />
+              </div>
+
+              <p className="text-center text-xs text-[#ff6865]">
+                <a
+                  href="https://www.vectorstock.com/royalty-free-vector/teacher-sitting-classroom-with-chalkboard-vector-43444042"
+                  target="_blank"
+                >
+                  img source
+                </a>
+              </p>
+            </div>
+
             {/* three sections */}
             <div className="grid grid-flow-col grid-rows-4 gap-10 text-center ">
-              <div className="row-span- col-span-1 rounded-md border-2 border-gray-500 bg-[#e1f3ff]">
-                <div className="bg-[#a8cd87] py-1 font-semibold text-[#132241]">
+              <div className="col-span-1 row-span-1 rounded-md border border-gray-200 bg-white">
+                <div className="rounded-t-md bg-[#e1f3ff] py-1 font-semibold">
                   Teacher Info
                 </div>
                 <table className="flex table-auto flex-col items-center justify-center">
@@ -78,35 +96,37 @@ export default function Home() {
                   </tbody>
                 </table>
               </div>
-              <div className="col-span-1 inline-grid rounded-lg border-2 border-gray-500 bg-[#e1f3ff] ">
-                <div className="bg-[#a8cd87] py-1 font-semibold text-[#132241] ">
+
+              <div className="col-span-1 row-span-1 rounded-md border border-gray-200 bg-white">
+                <div className="rounded-t-md bg-[#e1f3ff] py-1 font-semibold">
                   Actions
                 </div>
                 <table className="flex table-auto flex-col items-center justify-center">
                   <tbody>
-                    <tr className="cursor-pointer hover:text-[#5c6ac4]">
+                    <tr className="cursor-pointer hover:text-[#ff6865]">
                       {/* create a Tname option that changes according to Tname */}
                       <td>Change Classroom</td>
                     </tr>
-                    <tr className="cursor-pointer hover:text-[#5c6ac4]">
+                    <tr className="cursor-pointer hover:text-[#ff6865]">
                       {/* create a class naming option so class name changes depending on classroom */}
                       <td>Add Student</td>
                     </tr>
-                    <tr className="cursor-pointer hover:text-[#5c6ac4]">
+                    <tr className="cursor-pointer hover:text-[#ff6865]">
                       {/* create a pin option so students can join classroom */}
                       <td>Remove Student</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-              <div className="col-span-1 row-span-7 rounded-lg border-2 border-gray-500 bg-[#e1f3ff]">
-                <div className="bg-[#a8cd87] py-1 font-semibold text-[#132241]">
+
+              <div className="col-span-1 row-span-3 rounded-md border border-gray-200 bg-white">
+                <div className="rounded-t-md bg-[#e1f3ff] py-1 font-semibold">
                   Students
                 </div>
                 {/* ADD Student Feature */}
-                <table className="flex table-auto flex-col border-b border-gray-500">
+                <table className="flex table-auto flex-col border-b border-gray-200">
                   <tbody>
-                    <tr className="flex border-b border-gray-500">
+                    <tr className="flex border-b border-gray-200">
                       <td>John Doe</td>
                     </tr>
                     <tr>
@@ -116,12 +136,6 @@ export default function Home() {
                 </table>
               </div>
             </div>
-
-            <div className="relative h-32 w-32 [310px] ">
-              <div className="absolute bottom-[35px] left-[100px] h-[490px] w-[490px]"><img src="https://cdn3.vectorstock.com/i/1000x1000/40/42/teacher-sitting-classroom-with-chalkboard-vector-43444042.jpg"/>
-              <p className="">
-              <a href="https://www.vectorstock.com/royalty-free-vector/teacher-sitting-classroom-with-chalkboard-vector-43444042">Vector image by VectorStock / HappyDwiS</a></p></div>
-              </div>
           </div>
         </div>
       </section>
