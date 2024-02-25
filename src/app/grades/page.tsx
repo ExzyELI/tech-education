5cbb8970349d455652f097a82d9a8eb8804e843b
majedau <majedaullah@gmail.com>
2024-02-25 14:05:18 -0500

    Updated grades page to pull and display the activity data from the database

--- src/app/grades/page.tsx
+++ src/app/grades/page.tsx
@@ -1,127 +1,116 @@
 "use client";
-import { auth,useHandleRedirect } from "@/app/firebase/init_app";
-import { useRouter } from "next/navigation";
-import Footer from "../../../comps/footer";
-import Nav from "../../../comps/nav";
 import React, { useState, useEffect } from "react";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { User } from "firebase/auth";
+import Nav from "../../../comps/nav";
+import Footer from "../../../comps/footer";
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+import { auth } from "../firebase/init_app";
 
-export default function ActivitiesPage() {
-  const router = useRouter();
-  useHandleRedirect();
-
-  const [user, setUser] = useState<User | null>(null); // logged-in user
-  const [firstName, setFirstName] = useState(""); // first name
-  const [lastName, setLastName] = useState(""); // last name
+const GradesPage = () => {
+  const [user, setUser] = useState<User | null>(null); // setting user as User
+  const [grades, setGrades] = useState<any[]>([]); // setting fetchedGrades as any[]
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      // updating user state when auth state changes
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
-
       if (user) {
-        const firestore = getFirestore();
-
-        // reference to user document in Firestore
-        const userDocRef = doc(firestore, "users", user.uid);
-
-        // snapshot of user document
-        const userDocSnap = await getDoc(userDocRef);
-
-        if (userDocSnap.exists()) {
-          // user data is grabbed from snapshot if user document exists
-          const userData = userDocSnap.data();
-
-          // setting the state with user's first name, last name, and email
-          setFirstName(userData.firstName);
-          setLastName(userData.lastName);
-        }
+        fetchGrades(user.uid);
       }
     });
 
     return () => unsubscribe();
-    }, []);
+  }, []);
+
+  const fetchGrades = async (userId: string) => {
+    try {
+      const firestore = getFirestore();
+      const gradesRef = collection(firestore, `users/${userId}/activities`);
+      const q = query(gradesRef, orderBy("timestamp", "desc"));
+      const querySnapshot = await getDocs(q);
+
+      const fetchedGrades: any[] = [];
+      querySnapshot.forEach((doc) => {
+        fetchedGrades.push(doc.data());
+      });
+
+      setGrades(fetchedGrades);
+    } catch (error) {
+      console.error("Error fetching grades:", error);
+    }
+  };
 
   return (
-    <div className="font-family: flex min-h-screen flex-col bg-[#ffecde] font-serif leading-normal tracking-normal text-[#132241]">
-      {/*navbar begins */}
+    <main className="flex min-h-screen flex-col bg-[#ffecde] font-serif text-[#2d2d2d]">
+      <title>Grades</title>
       <Nav />
-      {/* navbar ends */}
-
-      <section className="container mx-auto w-full py-5">
-        <h1 className="flex flex-col items-center justify-center pt-4 text-5xl font-bold tracking-tight">
-          Grades
-        </h1>
-        <h2 className="font-sm items-center text-center text-2xl text-[#ff6865]">
-            {firstName} {lastName}
-        </h2>
-        <div className="mx-auto mb-10 mt-5 flex w-5/6 items-center justify-center rounded border bg-white p-8 lg:w-2/3">
-          <div className="gradient font-family: font-serif leading-normal tracking-normal">
-            <h3 className="mb-3 ml-3 text-lg font-semibold">Activities</h3>
-            <ul className="w-[250px] md:w-[350px] lg:w-[500px]">
-              {/* array for activity colors for odds and evens */}
-              {Array(10)
-                .fill(true)
-                .map((_, index) => {
-                  if (index % 2 == 0) {
-                    return (
-                      <div
-                        key={index}
-                        className="m-2 rounded-md border bg-[#e1f3ff] p-2"
-                      >
-                        Activity {index + 1}
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div
-                        key={index}
-                        className="m-2 rounded-md border bg-[#e8f8da] p-2"
-                      >
-                        Activity {index + 1}
-                      </div>
-                    );
-                  }
-                })}
-            </ul>
-          </div>
-
-          <div className="gradient font-family: font-serif leading-normal tracking-normal text-[#132241]">
-            <h3 className="mb-3 ml-3 text-left text-lg font-semibold">
-              Grades
-            </h3>
-            <ul className="w-[90px]">
-              {/* array for grades to have a different color for odds and evens */}
-              {Array(10)
-                .fill(true)
-                .map((_, index) => {
-                  if (index % 2 == 0) {
-                    return (
-                      <div
-                        key={index}
-                        className="m-2 rounded-md border bg-[#e1f3ff] p-2 text-center"
-                      >
-                        5/10
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div
-                        key={index}
-                        className="m-2 rounded-md border bg-[#e8f8da] p-2 text-center"
-                      >
-                        10/10
-                      </div>
-                    );
-                  }
-                })}
-            </ul>
+      <div className="mx-auto my-10 max-w-full flex-grow px-2 py-4 sm:px-4 md:px-8">
+        <div className="w-full max-w-7xl overflow-x-auto">
+          <div className="overflow-x-auto rounded-lg bg-white shadow-md">
+            {grades.length > 0 ? (
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-[#ff6865]">
+                  <tr>
+                    <th className="whitespace-nowrap px-2 py-2 text-left text-lg font-semibold uppercase tracking-wider text-white sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                      Activity
+                    </th>
+                    <th className="whitespace-nowrap px-2 py-2 text-left text-lg font-semibold uppercase tracking-wider text-white sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                      Score
+                    </th>
+                    <th className="whitespace-nowrap px-2 py-2 text-left text-lg font-semibold uppercase tracking-wider text-white sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                      Time
+                    </th>
+                    <th className="whitespace-nowrap px-2 py-2 text-left text-lg font-semibold uppercase tracking-wider text-white sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                      Attempt #
+                    </th>
+                    <th className="whitespace-nowrap px-2 py-2 text-left text-lg font-semibold uppercase tracking-wider text-white sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                      Date
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {grades.map((grade, index) => (
+                    <tr
+                      key={index}
+                      className={index % 2 === 0 ? "bg-[#fff7f7]" : "bg-white"}
+                    >
+                      <td className="whitespace-nowrap px-2 py-2 text-lg font-semibold text-gray-900 sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                        {grade.activityName}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-lg text-gray-500 sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                        {grade.score}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-lg text-gray-500 sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                        {grade.elapsedTime}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-lg text-gray-500 sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                        {grade.attempts}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-lg text-gray-500 sm:px-4 sm:py-4 md:px-8 md:text-xl">
+                        {grade.timestamp
+                          ? grade.timestamp.toDate().toLocaleString()
+                          : ""}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <div className="p-6 text-center text-lg text-[#ff6865]">
+                No grades available
+              </div>
+            )}
           </div>
         </div>
-      </section>
-
+      </div>
       <Footer />
-    </div>
+    </main>
   );
-}
+};
+
+export default GradesPage;
