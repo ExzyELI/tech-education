e3e2533259a5aa7908d5ec2cb43bf7a82e7f5d2a
Andre-N <andrehadnan@yahoo.com>
2024-03-01 01:42:40 -0500

    Added press Right Arrow or Left Arrow key activity

---
+++ src/app/KeyboardActivity-kindergarten/KeyboardActivity-ArrowRightLeft/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Nav from "../../../../comps/nav";
+import Footer from "../../../../comps/footer";
+import React, { useState, useEffect } from "react";
+
+const arrowsKeyPressPage = () => {
+    //store the last key pressed
+    const [keyPressed, setKeyPressed] = useState("");
+    //track if the correct key was pressed
+    const [correctPress, setCorrectPress] = useState(false);
+    //track if the marks should be shown 
+    const [showMarks, setShowMarks] = useState(false);
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            //check if the arrows button is pressed
+            if (e.code === "ArrowRight") {
+                setKeyPressed("ArrowRight");
+                setCorrectPress(true);
+                setShowMarks(true);
+            }
+            else if (e.code === "ArrowLeft") {
+                setKeyPressed("ArrowLeft");
+                setCorrectPress(true);
+                setShowMarks(true);
+                
+            } else {
+                //if any other key is pressed
+                setKeyPressed(e.code);
+                setCorrectPress(false);
+                setShowMarks(true);
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <main className="flex min-h-screen flex-col space-y-[110px] bg-[#ffecde] font-serif leading-normal tracking-normal text-[#132241]">
+            <title>Press Arrow Up or Arrow Down Activity</title>
+            <Nav />
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold">Press Arrow Up or Arrow Down Buttons</h1>
+                {/* image path */}
+                <img src="/images/ArrowRightArrowLeft-key-press.gif" alt="Press arrows" className="mt-4" />
+                {showMarks && (
+                    <p className="mt-4 text-lg">
+                        {/* display if right key is pressed */}
+                        {correctPress ? (
+                            <span className="text-green-500">✓ Correct! You pressed the {keyPressed} button.</span>
+                        ) : (
+                            //display any other key pressed */}
+                            <span className="text-red-500">✗ You pressed {keyPressed}. Please press arrow up or arrow down buttons.</span>
+                        )}
+                    </p>
+                )}
+            </div>
+            <Footer />
+        </main>
+    );
+};
+
+export default arrowsKeyPressPage;
