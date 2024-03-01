ca68ae01d1144b5998f72b76299a1dc9ef61686a
Andre-N <andrehadnan@yahoo.com>
2024-03-01 01:44:09 -0500

    Added press Ctrl right or Ctrl Left key activity

---
+++ src/app/KeyboardActivity-kindergarten/KeyboardActivity-ctrl/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Nav from "../../../../comps/nav";
+import Footer from "../../../../comps/footer";
+import React, { useState, useEffect } from "react";
+
+const ctrlKeyPressPage = () => {
+    //store the last key pressed
+    const [keyPressed, setKeyPressed] = useState("");
+    //track if the correct key was pressed
+    const [correctPress, setCorrectPress] = useState(false);
+    //track if the marks should be shown 
+    const [showMarks, setShowMarks] = useState(false);
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            //check if the ctrl button is pressed
+            if (e.code === "ControlLeft") {
+                setKeyPressed("ControlLeft");
+                setCorrectPress(true);
+                setShowMarks(true);
+            }
+            else if (e.code === "ControlRight") {
+                setKeyPressed("ControlRight");
+                setCorrectPress(true);
+                setShowMarks(true);
+                
+            } else {
+                //if any other key is pressed
+                setKeyPressed(e.code);
+                setCorrectPress(false);
+                setShowMarks(true);
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <main className="flex min-h-screen flex-col space-y-[110px] bg-[#ffecde] font-serif leading-normal tracking-normal text-[#132241]">
+            <title>Press any of the ctrl Activity</title>
+            <Nav />
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold">Press ctrlLeft or ctrlRight Button</h1>
+                {/* image path */}
+                <img src="/images/ctrl-key-press.gif" alt="Press ctrl" className="mt-4" />
+                {showMarks && (
+                    <p className="mt-4 text-lg">
+                        {/* display if right key is pressed */}
+                        {correctPress ? (
+                            <span className="text-green-500">✓ Correct! You pressed the {keyPressed} button.</span>
+                        ) : (
+                            //display any other key pressed */}
+                            <span className="text-red-500">✗ You pressed {keyPressed}. Please press any of ctrl buttons.</span>
+                        )}
+                    </p>
+                )}
+            </div>
+            <Footer />
+        </main>
+    );
+};
+
+export default ctrlKeyPressPage;
