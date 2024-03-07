7c48f5fe3eb07d51de32c1571528a5af26a7ea39
ExzyELI <Adeadeniji133@gmail.com>
2024-03-07 16:40:31 -0500

    Updating the Kindergarten and 2nd grade matching games to send data to the database.

--- src/app/MatchingGameK/page.tsx
+++ src/app/MatchingGameK/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import { useState, useEffect } from "react";
 import { shuffle } from "lodash"; // npm run lodash
 import MatchingNavBar from "../MatchingGame/MatchingNavBar";
@@ -24,15 +24,14 @@ import { User } from "firebase/auth";
 export default function Home() {
   const gridSize = 2;
   const totalCards = gridSize * (gridSize + 1); //amount of squares
-  
+
   const images = [
     "/CGimages/mouse.png",
     "/CGimages/keyboard.png",
     "/CGimages/monitor.png",
-    
   ];
   const initialCards = Array.from(Array(totalCards).keys()).flatMap((num) => [
-    num % images.length // Store the index of the image in the images array
+    num % images.length, // Store the index of the image in the images array
   ]);
   const [cards, setCards] = useState<number[]>(shuffle(initialCards));
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
@@ -44,19 +43,18 @@ export default function Home() {
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [user, setUser] = useState<User | null>(null); // store logged in user
-  const [matching1_attempts, setAttempts] = useState(0); // store attempts
+  const [matchingK_attempts, setAttempts] = useState(0); // store attempts
   let interval: string | number | NodeJS.Timeout | undefined;
 
   useEffect(() => {
-  
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user); // update the user state when auth state changes
-      console.log("Can you see this?")
+      console.log("Can you see this?");
     });
 
     return () => unsubscribe();
   }, []);
-  
+
   useEffect(() => {
     if (isActive) {
       interval = setInterval(() => {
@@ -80,7 +78,6 @@ export default function Home() {
 
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   };
-  
 
   const handleStart = () => {
     setIsActive(true);
@@ -104,30 +101,34 @@ export default function Home() {
   const handleSave = async () => {
     console.log(user);
     const firestore = getFirestore();
-      if (user) {
-        const userDocRef = doc(firestore, `users/${user.uid}`);
-        const userDocSnap = await getDoc(userDocRef);
-        if (userDocSnap.exists()) {
-          const userData = userDocSnap.data();
-          const currentAttempts = userData?.matching1_attempts || 0; // 0 if no attempts found
-          // update attempts locally
-          setAttempts(currentAttempts + 1);
-          // update attempts to firestore
-          await setDoc(userDocRef, { matching1_attempts: increment(1) }, { merge: true });
-          // save activity data in firestore
-          await addDoc(collection(firestore, `users/${user.uid}/activities`), {
-            activityName: "Matching Activity",
-            score: finalScore,
-            matching1_attempts: currentAttempts + 1,
-            timestamp: new Date(),
-            elapsedTime: formatTime(seconds),
-          });
-          console.log("worked");
-        }
+    if (user) {
+      const userDocRef = doc(firestore, `users/${user.uid}`);
+      const userDocSnap = await getDoc(userDocRef);
+      if (userDocSnap.exists()) {
+        const userData = userDocSnap.data();
+        const currentAttempts = userData?.matchingK_attempts || 0; // 0 if no attempts found
+        // update attempts locally
+        setAttempts(currentAttempts + 1);
+        // update attempts to firestore
+        await setDoc(
+          userDocRef,
+          { matchingK_attempts: increment(1) },
+          { merge: true },
+        );
+        // save activity data in firestore
+        await addDoc(collection(firestore, `users/${user.uid}/activities`), {
+          activityName: "Matching Activity K",
+          score: finalScore,
+          matchingK_attempts: currentAttempts + 1,
+          timestamp: new Date(),
+          elapsedTime: formatTime(seconds),
+        });
+        console.log("worked");
       }
-      //toast.success("Score submitted successfully!"); // show success message
-  }
-  
+    }
+    //toast.success("Score submitted successfully!"); // show success message
+  };
+
   useEffect(() => {
     if (matchedCards.length === cards.length) {
       setIsGameWon(true);
@@ -137,7 +138,8 @@ export default function Home() {
     }
   }, [matchedCards, totalCards]);
 
-  const handleCardClick = (index: number) => { // Ignore clicks on matched cards
+  const handleCardClick = (index: number) => {
+    // Ignore clicks on matched cards
     if (
       matchedCards.includes(index) ||
       flippedCards.includes(index) ||
@@ -177,7 +179,8 @@ export default function Home() {
     }
   };
 
-  const resetGame = (): void => { // Reset button functions
+  const resetGame = (): void => {
+    // Reset button functions
     setCards(shuffle(initialCards));
     setFlippedCards([]);
     setMatchedCards([]);
@@ -188,7 +191,7 @@ export default function Home() {
     handleStop();
     handleReset();
   };
-  
+
   const startGame = () => {
     setGameStarted(true);
     setCards(shuffle(initialCards)); // Shuffle cards after game is in start
@@ -197,37 +200,31 @@ export default function Home() {
   const calculateScore = () => {
     const totalMoves = clickCount;
     const elapsedTime = seconds;
-    const score = (12000 - totalMoves * 100 - elapsedTime * 10)/2500;
-    if (score >= 4){
-      return 4 
-    }
-    else if (score >= 3.5){
-      return 3.5
-    }
-    else if (score >= 3){
-      return 3
-    }
-    else if (score >= 2.5){
-      return 2.5
-    }
-    else if (score >= 2){
-      return 2
-    }
-    else if (score >= 1.5){
-      return 1.5
-    }
-    else if (score >= 1){
-      return 1
+    const score = (12000 - totalMoves * 100 - elapsedTime * 10) / 2500;
+    if (score >= 4) {
+      return 4;
+    } else if (score >= 3.5) {
+      return 3.5;
+    } else if (score >= 3) {
+      return 3;
+    } else if (score >= 2.5) {
+      return 2.5;
+    } else if (score >= 2) {
+      return 2;
+    } else if (score >= 1.5) {
+      return 1.5;
+    } else if (score >= 1) {
+      return 1;
     }
     //return Math.max(score,0);
-  }
-  const finalScore = calculateScore()
-  
+  };
+  const finalScore = calculateScore();
+
   return (
     <main>
       <div className="font-sans leading-normal tracking-normal text-[#132241]">
         {/*navbar begins */}
-        <Nav/>
+        <Nav />
         {/* navbar ends */}
         <div className="flex min-h-screen bg-gradient-to-br from-[#FAF9F6] to-[#FAF9F6] text-[#434343]">
           <div className="container mx-auto max-w-3xl px-4 py-6">
@@ -237,61 +234,82 @@ export default function Home() {
             <p className="mt-2 text-center text-xl">
               Instructions: Click on cards to match them.
             </p>
-            <div className="mx-[290px]"> 
-            {!gameStarted && (
-              <button className="rounded-lg bg-[#ffe08d] shadow-md px-6 py-4 mt-4 text-xl transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#ff914d]" onClick={startGame}>Start Game</button>
-            )}
-          </div>
-          {gameStarted && (
-            <div>
-              <div className="mt-4 flex justify-evenly">
-                <p className="bg-[#5c93ff] text-white rounded-lg p-4 border-gray-300 border-4 cursor-none">Click Count: {clickCount}</p>
-                <p className="bg-[#5c93ff] text-white rounded-lg p-4 border-gray-300 border-4 w-33 cursor-none">Timer: {formatTime(seconds)}</p>
-              </div>
-              <div className="container mt-8 border-4 border-dashed border-[#5c93ff] bg-[#5ab2ff] px-3 py-2">
-        <section className="grid grid-cols-3 justify-items-center gap-3">
-          {cards.map((card, index) => (
-            <button
-            key={index}
-            className={`memory-card flex h-40 w-40 items-center justify-center rounded-lg bg-[#ff5a5f] text-white hover:bg-[#ff4146] focus:outline-none focus:ring focus:ring-black active:bg-[#ff4146] shadow-lg ${flippedCards.includes(index) || matchedCards.includes(index) ? "flipped" : ""}`}
-            onClick={() => handleCardClick(index)}
-            disabled={isDisabled || matchedCards.includes(index)}
-        >
-            {flippedCards.includes(index) || matchedCards.includes(index) ? (
-                <img
-                    src={images[card]}
-                    alt={`Card ${index}`}
-                    style={{ width: "100%", height: "100%"}}
-                />
-            ) : (
-                <img src="https://cdn.discordapp.com/attachments/1196945767785578598/1205159897663279114/Screen_Shot_2024-02-08_at_9.34.52_AM.png?ex=65f30b1e&is=65e0961e&hm=0f846bd73ee4c0bb03c39dc0488456a41e7ab937acd0f0824327bd152cd18dc7&"
-                
-                style={{ width: "75%", height: "75%"}} />
-            )}
-        </button>
-          ))}
-        </section>
-      </div>
-              <div className="mt-4 flex justify-center">
+            <div className="mx-[290px]">
+              {!gameStarted && (
                 <button
-                  className="rounded bg-red-500 px-4 py-2 z-10 font-bold text-white hover:bg-red-600 shadow-md"
-                  onClick={resetGame}>Reset Game </button>
-              </div>
-              {isGameWon && (
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="bg-green-500 bg-opacity-90 text-white rounded-lg p-8">
-                    <div className="text-6xl font-bold text-center">
-                      Great Job!
-                      <div className="flex items-center justify-center animate-bounce"> {/* Flex container */}
-                        <img src="/CGimages/goodjob-man (1).png" style={{ width: "350px", height: "350px" }} />
-                      </div>
-                    </div>
-                    <p className="bg-green-500 bg-opacity-60 text-white text-3xl rounded-lg p-1 text-center">Score: {finalScore}</p>
-                  </div>
-                </div>
+                  className="mt-4 rounded-lg bg-[#ffe08d] px-6 py-4 text-xl shadow-md transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#ff914d]"
+                  onClick={startGame}
+                >
+                  Start Game
+                </button>
               )}
             </div>
-          )}
+            {gameStarted && (
+              <div>
+                <div className="mt-4 flex justify-evenly">
+                  <p className="cursor-none rounded-lg border-4 border-gray-300 bg-[#5c93ff] p-4 text-white">
+                    Click Count: {clickCount}
+                  </p>
+                  <p className="w-33 cursor-none rounded-lg border-4 border-gray-300 bg-[#5c93ff] p-4 text-white">
+                    Timer: {formatTime(seconds)}
+                  </p>
+                </div>
+                <div className="container mt-8 border-4 border-dashed border-[#5c93ff] bg-[#5ab2ff] px-3 py-2">
+                  <section className="grid grid-cols-3 justify-items-center gap-3">
+                    {cards.map((card, index) => (
+                      <button
+                        key={index}
+                        className={`memory-card flex h-40 w-40 items-center justify-center rounded-lg bg-[#ff5a5f] text-white shadow-lg hover:bg-[#ff4146] focus:outline-none focus:ring focus:ring-black active:bg-[#ff4146] ${flippedCards.includes(index) || matchedCards.includes(index) ? "flipped" : ""}`}
+                        onClick={() => handleCardClick(index)}
+                        disabled={isDisabled || matchedCards.includes(index)}
+                      >
+                        {flippedCards.includes(index) ||
+                        matchedCards.includes(index) ? (
+                          <img
+                            src={images[card]}
+                            alt={`Card ${index}`}
+                            style={{ width: "100%", height: "100%" }}
+                          />
+                        ) : (
+                          <img
+                            src="https://cdn.discordapp.com/attachments/1196945767785578598/1205159897663279114/Screen_Shot_2024-02-08_at_9.34.52_AM.png?ex=65f30b1e&is=65e0961e&hm=0f846bd73ee4c0bb03c39dc0488456a41e7ab937acd0f0824327bd152cd18dc7&"
+                            style={{ width: "75%", height: "75%" }}
+                          />
+                        )}
+                      </button>
+                    ))}
+                  </section>
+                </div>
+                <div className="mt-4 flex justify-center">
+                  <button
+                    className="z-10 rounded bg-red-500 px-4 py-2 font-bold text-white shadow-md hover:bg-red-600"
+                    onClick={resetGame}
+                  >
+                    Reset Game{" "}
+                  </button>
+                </div>
+                {isGameWon && (
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="rounded-lg bg-green-500 bg-opacity-90 p-8 text-white">
+                      <div className="text-center text-6xl font-bold">
+                        Great Job!
+                        <div className="flex animate-bounce items-center justify-center">
+                          {" "}
+                          {/* Flex container */}
+                          <img
+                            src="/CGimages/goodjob-man (1).png"
+                            style={{ width: "350px", height: "350px" }}
+                          />
+                        </div>
+                      </div>
+                      <p className="rounded-lg bg-green-500 bg-opacity-60 p-1 text-center text-3xl text-white">
+                        Score: {finalScore}
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
